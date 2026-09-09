b3e13ebe868976340ea6b6777e81c5d9109df530
daniele-ottelli <danielematix67@gmail.com>
2026-09-09 16:10:24 +0200

    Enable indicator grounding for Ollama reports

--- app/workflow/assessment_workflow_factory.py
+++ app/workflow/assessment_workflow_factory.py
@@ -95,6 +95,7 @@ def create_workflow(
 
         llm_report_generator = LLMReportGenerator(
             llm_client=llm_client,
+            require_indicator_values=True,
         )
 
         reporting_agent = ReportingAgent(
