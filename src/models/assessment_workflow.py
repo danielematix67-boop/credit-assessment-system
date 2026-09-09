376163cb65541eb89a845314944696dd0ee09e1e
daniele-ottelli <danielematix67@gmail.com>
2026-09-09 12:41:44 +0200

    Add execution metadata to workflow result

--- src/models/assessment_workflow.py
+++ src/models/assessment_workflow.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from src.models.assessment import Assessment
 from src.models.assessment_analysis import AssessmentAnalysis
+from src.models.execution_metadata import ExecutionMetadata
 from src.models.report import Report
 
 
@@ -10,6 +11,7 @@ class AssessmentWorkflowResult:
     assessment: Assessment
     analysis: AssessmentAnalysis
     report: Report
+    execution_metadata: ExecutionMetadata | None = None
 
     report_generator_used: str | None = None
     report_generation_error: str | None = None
