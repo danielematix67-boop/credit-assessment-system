88138e86333bbdf5319541e819a6d424e685b7ea
daniele-ottelli <danielematix67@gmail.com>
2026-09-10 09:39:29 +0200

    refactor: add credit input package structure

---
+++ app/ui/input/__init__.py
@@ -0,0 +1,37 @@
+"""Credit position input UI and presentation metadata."""
+
+from app.ui.input.credit_position import (
+    build_credit_position_from_ui,
+    create_optional_numeric_field,
+    create_streamlit_field,
+    display_position_table,
+)
+from app.ui.input.field_metadata import (
+    FIELD_GROUPS,
+    build_scenario_data_table,
+    format_field_description,
+    format_field_label,
+    format_field_value,
+    get_credit_position_fields,
+    get_credit_position_type_hints,
+    get_field_default,
+    get_field_unit,
+    unwrap_optional,
+)
+
+__all__ = [
+    "FIELD_GROUPS",
+    "build_credit_position_from_ui",
+    "build_scenario_data_table",
+    "create_optional_numeric_field",
+    "create_streamlit_field",
+    "display_position_table",
+    "format_field_description",
+    "format_field_label",
+    "format_field_value",
+    "get_credit_position_fields",
+    "get_credit_position_type_hints",
+    "get_field_default",
+    "get_field_unit",
+    "unwrap_optional",
+]
