01a530c252f07537f9d2cdb3078c498f6ac2560f
daniele-ottelli <danielematix67@gmail.com>
2026-09-11 10:37:41 +0200

    fix: remove unused Rule import

--- src/rules/base/rule.py
+++ src/rules/base/rule.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Callable, ClassVar
+from typing import Callable, ClassVar
 
 from src.models.position import CreditPosition
 from src.rules.base.config import RuleConfig
