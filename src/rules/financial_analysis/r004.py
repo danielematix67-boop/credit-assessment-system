60361540ace7dbeac8ea29e364b9e9c092a7bf94
daniele-ottelli <danielematix67@gmail.com>
2026-09-14 12:35:54 +0200

    refactor: move R004 to unified financial rule layout

---
+++ src/rules/financial_analysis/r004.py
@@ -0,0 +1,25 @@
+from src.models.position import CreditPosition
+from src.rules.base.rule import Rule
+from src.rules.base.status import RuleStatus
+from src.rules.result import RuleResult
+
+
+@Rule.register("R004")
+class NfpToEbitdaRule(Rule):
+    def evaluate(
+        self,
+        position: CreditPosition,
+    ) -> RuleResult:
+        value, error = self._configured_value(position)
+
+        if error is not None:
+            return self._not_evaluable(reason=error)
+
+        assert value is not None
+        status = (
+            RuleStatus.TRIGGERED
+            if self._is_triggered(value)
+            else RuleStatus.NOT_TRIGGERED
+        )
+
+        return self._result(value=value, status=status)
